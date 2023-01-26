e8ba1c983cd43e016b710cab7acfee568cbf031a
rpahel <raphael.dacosta.pro@gmail.com>
2023-01-26 22:37:13 +0100

    Main menu animations

--- Assets/Team02/Scripts/Menus/MainMenuController.cs
+++ Assets/Team02/Scripts/Menus/MainMenuController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using DG.Tweening;
+using System.Collections;
 
 namespace Team02
 {
@@ -10,8 +12,8 @@ namespace Team02
     {
         [SerializeField] private TMP_Dropdown languageDropdown;
         [SerializeField] private Slider musicVolumeSlider;
-        [SerializeField] private Slider readSpeedSlider;
-        [SerializeField] private Toggle instantTextToggle;
+        //[SerializeField] private Slider readSpeedSlider;
+        //[SerializeField] private Toggle instantTextToggle;
         [SerializeField] private Button backButton;
 
         [Header("Menu Objects")]
@@ -19,6 +21,16 @@ namespace Team02
         [SerializeField] private GameObject optionsMenu;
         private AudioMixer audioMixer;
         private RapBattlesSO rapBattlesSo;
+
+        [Header("Title")]
+        [SerializeField] private RectTransform west;
+        [SerializeField] private RectTransform contenders;
+
+        [Header("For animations")]
+        [SerializeField] private RectTransform play;
+        [SerializeField] private RectTransform options;
+        [SerializeField] private RectTransform quit;
+        [SerializeField] private Image fonduAuNoir;
 
         private void Start()
         {
@@ -41,24 +53,32 @@ namespace Team02
                 musicVolumeSlider.onValueChanged.AddListener(GameOptions.SetMusicVolume);
             }
 
-            if (readSpeedSlider != null)
-            {
-                readSpeedSlider.value = GameOptions.readSpeed;
-                readSpeedSlider.onValueChanged.AddListener(GameOptions.SetReadingSpeed);
-            }
-
-            if (instantTextToggle != null)
-            {
-                instantTextToggle.isOn = GameOptions.isInstantText;
-                instantTextToggle.onValueChanged.AddListener(GameOptions.SetInstantText);
-            }
+            //if (readSpeedSlider != null)
+            //{
+            //    readSpeedSlider.value = GameOptions.readSpeed;
+            //    readSpeedSlider.onValueChanged.AddListener(GameOptions.SetReadingSpeed);
+            //}
+            //
+            //if (instantTextToggle != null)
+            //{
+            //    instantTextToggle.isOn = GameOptions.isInstantText;
+            //    instantTextToggle.onValueChanged.AddListener(GameOptions.SetInstantText);
+            //}
 
             backButton.onClick.AddListener(BackToMenu);
+            StartCoroutine(EnterAnimation());
         }
 
         #region mainMenu
         public void StartGame()
         {
+            StartCoroutine(FadeToStart());
+        }
+
+        private IEnumerator FadeToStart()
+        {
+            fonduAuNoir.DOColor(new Color(0, 0, 0, 1), 1);
+            yield return new WaitForSeconds(2);
             SceneManager.LoadScene(1);
         }
 
@@ -82,5 +102,31 @@ namespace Team02
         }
 
         #endregion optionsMenu
+
+        #region animations
+        private IEnumerator EnterAnimation()
+        {
+            Vector2 westStartPos = west.anchoredPosition;
+            Vector2 contendersStartPos = contenders.anchoredPosition;
+            west.anchoredPosition = westStartPos - 1500 * Vector2.left;
+            contenders.anchoredPosition = contendersStartPos + 1500 * Vector2.left;
+            Vector2 playStartPos = play.anchoredPosition;
+            Vector2 optionsStartPos = options.anchoredPosition;
+            Vector2 quitStartPos = quit.anchoredPosition;
+            play.anchoredPosition = playStartPos + 1000 * Vector2.left;
+            options.anchoredPosition = optionsStartPos + 1000 * Vector2.left;
+            quit.anchoredPosition = quitStartPos + 1000 * Vector2.left;
+            yield return new WaitForSeconds(1);
+            west.DOAnchorPos(westStartPos, .5f).SetEase(Ease.OutBack, 1);
+            yield return new WaitForSeconds(.25f);
+            contenders.DOAnchorPos(contendersStartPos, .5f).SetEase(Ease.OutBack, 1);
+            yield return new WaitForSeconds(.25f);
+            play.DOAnchorPos(playStartPos, .5f).SetEase(Ease.OutBack, 1);
+            yield return new WaitForSeconds(.25f);
+            options.DOAnchorPos(optionsStartPos, .5f).SetEase(Ease.OutBack, 1);
+            yield return new WaitForSeconds(.25f);
+            quit.DOAnchorPos(quitStartPos, .5f).SetEase(Ease.OutBack, 1);
+        }
+        #endregion
     }
 }
